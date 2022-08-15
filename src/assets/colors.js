1cce6cd2fa7678071be11ab6f1b1ac48eaaf42c3
Jess Carvalho <carvalhojessicag@gmail.com>
2022-08-15 00:55:17 -0700

    remove assets folder

--- src/assets/colors.js
+++
@@ -1,8 +0,0 @@
-export const colors = {
-  primary: "#2548c6",
-  secondary: "#3a53d4",
-  background: "#13181b",
-  backgroundSecondary: "#1a1d22",
-  textColor: '#ffffff',
-  activeColor: '#0C0C0C'
-}
